8a813ff99f0743fe8d3fab74ca9fde9ee97c4ec4
TGGamesYT <tothgergoci@gmail.com>
2025-08-26 15:45:39 +0200

    yes2

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+const path = require('path'); // ← add this
 const { app, BrowserWindow } = require('electron')
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1000,
     height: 700,
+    icon: path.join(__dirname, 'assets', 'icon.png'),
     webPreferences: {
       nodeIntegration: true
     }
